890698fb123b0cb45b292d487db2b34c5f21bee6
attackondemand <michael.e.silvestre@gmail.com>
2018-05-16 17:56:40 -0700

    Add project page

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ app.use((req, res, next) => {
   next();
 })
 
-app.use((req, res) => {
-  res.render('maintenance.hbs')
-
-})
+// app.use((req, res) => {
+//   res.render('maintenance.hbs')
+//
+// })
 
 app.use(express.static(__dirname + '/public'));
 
@@ -46,6 +46,13 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) =>{
   res.render('about.hbs', {
     pageTitle: 'About Page',
+  });
+})
+
+app.get('/projects', (req, res) =>{
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    pageMessage: 'Portfolio page here'
   });
 })
 
